6b04bb92e1c8748552b360c95119f9f73844950f
Darthsoviet <31465914+Darthsoviet@users.noreply.github.com>
2021-03-08 18:56:45 -0600

    ejercicio done

---
+++ inventariosApp/models/User.js
@@ -0,0 +1,13 @@
+const login=(email,oasswd,callback)=>{
+   let err=""
+   let bdData=''
+   if(email=="jhon@doe.com" && pass=="1234"){   
+      bdData={
+         email:"email"
+      }
+   }else{
+
+   }
+}
+
+exports.login=login
